f0572022f52bfe4135af8a46dbbd5cb52ac85f78
yassir <elatmaniyassir3@gmail.com>
2023-11-28 23:36:06 +0100

    Small ui teak

--- src/app/_components/List.tsx
+++ src/app/_components/List.tsx
@@ -17,7 +17,7 @@ export const List:React.FC<props> = ({taskType,taskStatus, color,  boardId}) =>{
     return (<div className='max-w-[300px] min-w-[250px] flex flex-col gap-6'>
         <div className="flex gap-3 justify-start items-center">
             <GoDotFill fontSize='18px' color={color}/>
-            <p className='font-semi-bold text-[#C4C1BB] text-l'>{taskType}</p>
+            <p className='font-semi-bold text-[#C4C1BB] text-l'>{tasks?.length ? `${taskType} (${tasks?.length})` : taskType}</p>
         </div>
         <div className='w-[98%] flex flex-col gap-4 overflow-auto max-h-[55vh]'>
             {tasks && tasks!.map ((item:any, index:number)=>{
